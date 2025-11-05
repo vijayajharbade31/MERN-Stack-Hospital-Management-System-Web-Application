4cf8a9e5d3be5a850fa563b27cd768821219f133
vijayajharbade31 <158267734+vijayajharbade31@users.noreply.github.com>
2025-11-05 21:34:17 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
